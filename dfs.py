f4c381fad6e0385af3461882e750bc5660fd6750
ahlusar1989 <saran@divvycloud.com>
2016-10-26 19:15:00 -0400

    added algorithm for 5 top components

--- dfs.py
+++ dfs.py
@@ -1,10 +1,48 @@
-from collections import defaultdict
-import resource
+#This script computes the strong connected components(SCC) of a given graph.
 import sys
+import time
+import heapq
+import resource
+from itertools import groupby
+from collections import defaultdict
 
-#set rescursion limit and stack size limit
-# sys.setrecursionlimit(10 ** 6)
-# resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, 2 ** 30))
+# This program shows off a python decorator(
+# which implements tail call optimization. It
+# does this by throwing an exception if it is 
+# it's own grandparent, and catching such 
+# exceptions to recall the stack.
+
+
+class TailRecurseException:
+  def __init__(self, args, kwargs):
+    self.args = args
+    self.kwargs = kwargs
+
+def tail_call_optimized(g):
+  """
+  This function decorates a function with tail call
+  optimization. It does this by throwing an exception
+  if it is it's own grandparent, and catching such
+  exceptions to fake the tail call optimization.
+
+  This function fails if the decorated
+  function recurses in a non-tail context.
+  """
+  def func(*args, **kwargs):
+    f = sys._getframe()
+    if f.f_back and f.f_back.f_back \
+        and f.f_back.f_back.f_code == f.f_code:
+      raise TailRecurseException(args, kwargs)
+    else:
+      while 1:
+        try:
+          return g(*args, **kwargs)
+        except TailRecurseException, e:
+          args = e.args
+          kwargs = e.kwargs
+  func.__doc__ = g.__doc__
+  return func
+
 
 class Tracker(object):
     """Keeps track of the current time, current source, component leader,
@@ -19,33 +57,12 @@ class Tracker(object):
         self.finish_time = {}
         self.explored = set()
 
-
-
-def read_graph(input_file, reverse=False):
-
-    graph = defaultdict(list)
-    with open('SCC.txt') as file_in:
-        for line in file_in:
-            x = line.strip().split()
-            x1, x2 = int(x[0]), int(x[1])
-            graph[x1].append(x2)
-    return graph
-
-def graph_reverse(graph):
-    """Given a directed graph in forms of {tail:[head_list], ...}, compute
-    a reversed directed graph, in which every edge changes direction."""
-
-    reversed_graph = defaultdict(list)
-    for tail, head_list in graph.items():
-        for head in head_list:
-            reversed_graph[head].append(tail)
-    return reversed_graph
-
-
+@tail_call_optimized
 def dfs(graph_dict, node, tracker):
     """Inner loop explores all nodes in a SCC. Graph represented as a dict,
     {tail: [head_list], ...}. Depth first search runs recursively and keeps
     track of the parameters"""
+
     tracker.explored.add(node)
     tracker.leader[node] = tracker.current_source
     for head in graph_dict[node]:
@@ -57,17 +74,72 @@ def dfs(graph_dict, node, tracker):
 def dfs_loop(graph_dict, nodes, tracker):
     """Outer loop checks out all SCCs. Current source node changes when one
     SCC inner loop finishes."""
+
     for node in nodes:
         if node not in tracker.explored:
             tracker.current_source = node
             dfs(graph_dict, node, tracker)
 
-def main():
-    filename = 'SCC.txt'
-    #filename = 'test2.txt'
-    G = read_graph(filename)
 
+def graph_reverse(graph):
+    """Given a directed graph in forms of {tail:[head_list], ...}, compute
+    a reversed directed graph, in which every edge changes direction."""
+
+    reversed_graph = defaultdict(list)
+    for tail, head_list in graph.items():
+        for head in head_list:
+            reversed_graph[head].append(tail)
+    return reversed_graph
+
+@tail_call_optimized
+def scc(graph):
+    """First runs dfs_loop on reversed graph with nodes in decreasing order,
+    then runs dfs_loop on original graph with nodes in decreasing finish
+    time order(obtained from first run). Return a dict of {leader: SCC}."""
+
+    out = defaultdict(list)
+    tracker1 = Tracker()
+    tracker2 = Tracker()
+    nodes = set()
+    reversed_graph = graph_reverse(graph)
+    for tail, head_list in graph.items():
+        nodes |= set(head_list)
+        nodes.add(tail)
+    nodes = sorted(list(nodes), reverse=True)
+    dfs_loop(reversed_graph, nodes, tracker1)
+    sorted_nodes = sorted(tracker1.finish_time,
+                          key=tracker1.finish_time.get, reverse=True)
+    dfs_loop(graph, sorted_nodes, tracker2)
+    for lead, vertex in groupby(sorted(tracker2.leader, key=tracker2.leader.get),
+                                key=tracker2.leader.get):
+        out[lead] = list(vertex)
+    return out
+
+
+def main():
+    start = time.time()
+    graph = defaultdict(list)
+    with open('SCC.txt') as file_in:
+    #with open('test.txt') as file_in:
+        for line in file_in:
+            x = line.strip().split()
+            x1, x2 = int(x[0]), int(x[1])
+            graph[x1].append(x2)
+    t1 = time.time() - start
+    print t1
+    groups = scc(graph)
+    top_5 = heapq.nlargest(5, groups, key=lambda x: len(groups[x]))
+    t2 = time.time() - start
+    print t2
+    # #sorted_groups = sorted(groups, key=lambda x: len(groups[x]), reverse=True)
+    result = []
+    for i in range(5):
+        try:
+            result.append(len(groups[top_5[i]]))
+        except:
+            result.append(0)
+    print result
 
 
 if __name__ == '__main__':
-    main()
+    print main()
